7f28a8ffb773c810900b20bff5b0ba02a5b78fcf
adust09 <shoukitsuda@icloud.com>
2021-10-27 22:49:43 +0900

    add:src/core/groups/poap

---
+++ src/core/groups/poap/index.ts
@@ -0,0 +1,13 @@
+import getPoapGroupNamesByAddress from "./getPoapGroupNamesByAddress";
+import getPoapEventFancyId from "./getPoapEventFancyId";
+import getPoapEventName from "./getPoapEventName";
+import PoapGroupName from "./poapGroupName";
+import getPoapGroupNames from "./getPoapGroupNames";
+
+export {
+  getPoapGroupNamesByAddress,
+  getPoapEventFancyId,
+  PoapGroupName,
+  getPoapGroupNames,
+  getPoapEventName,
+};
