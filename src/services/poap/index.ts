ef7a7c9cb17b18bb5070756b38135dfee04be217
adust09 <shoukitsuda@icloud.com>
2021-10-28 02:25:01 +0900

    add:services/poap

---
+++ src/services/poap/index.ts
@@ -0,0 +1,6 @@
+export async function getPoapTokens(ethereumAddress: string) {
+  const response = await fetch(
+    `https://api.poap.xyz/actions/scan/${ethereumAddress}`
+  );
+  return response.json();
+}
