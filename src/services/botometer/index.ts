0b93fa1bba542d9510d9758f4318b3434e827c4a
adust09 <shoukitsuda@icloud.com>
2021-11-04 20:22:31 +0900

    add: src/services

---
+++ src/services/botometer/index.ts
@@ -0,0 +1,17 @@
+import { Botometer } from "botometer";
+import config from "src/config";
+
+const botometer = new Botometer({
+  consumerKey: config.TWITTER_CONSUMER_KEY,
+  consumerSecret: config.TWITTER_CONSUMER_SECRET,
+  accessToken: config.TWITTER_ACCESS_TOKEN,
+  accessTokenSecret: config.TWITTER_ACCESS_TOKEN_SECRET,
+  rapidApiKey: config.RAPIDAPI_KEY,
+  usePro: true,
+});
+
+export default async function getBotometerScore(
+  username: string
+): Promise<any> {
+  return botometer.getScore(username);
+}
